39dc1c03413a29e1674734a5ba33a1bf3492c67b
corpi <lj845854@gmail.com>
2024-01-11 23:35:26 +0900

    feat: NoteModel 생성

---
+++ lib/domain/model/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  String id;
+  String colorHex;
+  String title;
+  String content;
+
+  NoteModel({
+    required this.id,
+    required this.colorHex,
+    required this.title,
+    required this.content,
+  });
+}
