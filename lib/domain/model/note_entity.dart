3a191734a26789961a889c60f53f8dab2ae5bcbd
corpi <lj845854@gmail.com>
2024-01-11 23:34:07 +0900

    feat: NoteEntity 생성

---
+++ lib/domain/model/note_entity.dart
@@ -0,0 +1,13 @@
+class NoteEntity {
+  String id;
+  String colorHex;
+  String title;
+  String content;
+
+  NoteEntity({
+    required this.id,
+    required this.colorHex,
+    required this.title,
+    required this.content,
+  });
+}
