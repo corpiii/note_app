7fe582eb155526c68463e97a2b4266f72aef22b5
corpi <lj845854@gmail.com>
2024-01-12 00:10:45 +0900

    feat: Translator 인터페이스 정의 및 ModelTranslator 생성

---
+++ lib/domain/translator/model_translator.dart
@@ -0,0 +1,25 @@
+import 'package:note_app/domain/model/note_entity.dart';
+import 'package:note_app/domain/model/note_model.dart';
+import 'package:note_app/domain/translator/interface/translator.dart';
+
+class ModelTranslator implements Translator<NoteEntity, NoteModel> {
+  @override
+  NoteEntity translateFrom(NoteModel data) {
+    return NoteEntity(
+        id: data.id,
+        colorHex: data.colorHex,
+        title: data.title,
+        content: data.content
+    );
+  }
+
+  @override
+  NoteModel translateTo(NoteEntity data) {
+    return NoteModel(
+        id: data.id,
+        colorHex: data.colorHex,
+        title: data.title,
+        content: data.content
+    );
+  }
+}
