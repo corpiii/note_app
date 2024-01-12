e4cad3bdfa1d7e6cd51ca07cd0aa8fbacce2a0b8
corpi <lj845854@gmail.com>
2024-01-12 19:59:23 +0900

    refactor: NoteCreatableRepository 내 불필요한 getter 삭제

--- lib/domain/repository/note_creatable_repository.dart
+++ lib/domain/repository/note_creatable_repository.dart
@@ -3,8 +3,5 @@ import 'package:note_app/domain/model/note_entity.dart';
 import '../model/result.dart';
 
 abstract interface class NoteCreatableRepository {
-  Stream<List<NoteEntity>> get noteListStream;
-
-  Future<Result<NoteEntity>> create(
-      String backgroundColorHex, String fontHex, String title, String content);
+  Future<Result<NoteEntity>> create(String backgroundColorHex, String fontHex, String title, String content);
 }
