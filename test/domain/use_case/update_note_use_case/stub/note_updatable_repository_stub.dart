b7d3b09edc1a1eda69230e763d92049814d66bea
corpi <lj845854@gmail.com>
2024-01-12 23:39:30 +0900

    test: UpdateNoteUseCase test

---
+++ test/domain/use_case/update_note_use_case/stub/note_updatable_repository_stub.dart
@@ -0,0 +1,12 @@
+import 'package:note_app/domain/model/note_entity.dart';
+import 'package:note_app/domain/model/result.dart';
+import 'package:note_app/domain/repository/note_updatable_repository.dart';
+
+import '../../dummy/note_entity_list_dummy.dart';
+
+class NoteUpdatableRepositoryStub implements NoteUpdatableRepository {
+  @override
+  Future<Result<NoteEntity>> update(NoteEntity entity) async {
+    return Result.success(NoteEntityDummy.data[0]);
+  }
+}
