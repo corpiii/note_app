7e126fe60e13dc8fce7fa76fe6ed170f285673e6
corpi <lj845854@gmail.com>
2024-01-12 20:36:59 +0900

    test: NoteFetchableRepositoryStub, NoteEntityListDummy 생성 후 FetchNoteUseCase 테스트

---
+++ test/domain/use_case/fetch_note_use_case/stub/note_fetchable_repository_stub.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:note_app/domain/model/note_entity.dart';
+import 'package:note_app/domain/repository/note_fetchable_repository.dart';
+
+import '../dummy/note_entity_list_dummy.dart';
+
+class NoteFetchableRepositoryStub implements NoteFetchableRepository {
+  final StreamController<List<NoteEntity>> _controller = StreamController();
+
+  @override
+  void fetch() {
+    final dummy = NoteEntityDummy.data;
+
+    _controller.add(dummy);
+  }
+
+  @override
+  Stream<List<NoteEntity>> get noteListStream => _controller.stream;
+}
