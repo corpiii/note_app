1ea7723df052396362640c556be6ef5338708319
corpi <lj845854@gmail.com>
2024-01-14 14:58:31 +0900

    feat: HiveRepository 내 create 메서드 생성

---
+++ lib/data/hive_repository/repository/hive_repository.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note_app/data/hive_repository/dao/hive_note_dao.dart';
+import 'package:note_app/domain/model/note_entity.dart';
+import 'package:note_app/domain/model/result.dart';
+import 'package:note_app/domain/repository/note_repository.dart';
+
+class HiveRepository implements NoteRepository {
+  static final _boxName = 'noteBox';
+
+  final StreamController<Result<List<NoteEntity>>> _streamController = StreamController();
+
+  @override
+  Stream<Result<List<NoteEntity>>> get noteListStream => _streamController.stream;
+
+  @override
+  Future<Result<NoteEntity>> create({required NoteEntity entity}) async {
+    final box = await Hive.openBox(_boxName);
+
+    final dao = HiveNoteDAO(
+        id: entity.id,
+        backgroundColorHex: entity.backgroundColorHex,
+        fontColorHex: entity.fontColorHex,
+        title: entity.title,
+        content: entity.content);
+
+    try {
+      await box.add(dao);
+      return Result.success(entity);
+    } catch (_) {
+      return Result.error('fail to create note');
+    }
+  }
+
+  @override
+  Future<Result<NoteEntity>> delete({required String id}) {}
+
+  @override
+  void fetch() {}
+
+  @override
+  Future<Result<NoteEntity>> update(NoteEntity entity) {}
+}
--- lib/domain/repository/isp/note_creatable_repository.dart
+++ lib/domain/repository/isp/note_creatable_repository.dart
@@ -3,10 +3,5 @@ import 'package:note_app/domain/model/note_entity.dart';
 import '../../model/result.dart';
 
 abstract interface class NoteCreatableRepository {
-  Future<Result<NoteEntity>> create({
-    required String backgroundColorHex,
-    required String fontColorHex,
-    required String title,
-    required String content
-  });
+  Future<Result<NoteEntity>> create({required NoteEntity entity});
 }
