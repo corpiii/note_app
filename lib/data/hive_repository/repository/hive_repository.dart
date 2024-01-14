4ddc3d3d4e6b8129fe56e4ce47e50b558d5d8fc2
corpi <lj845854@gmail.com>
2024-01-14 15:39:19 +0900

    feat: repository fetch 메서드 구현 및 entityMapper 인터페이스 정의

--- lib/data/hive_repository/repository/hive_repository.dart
+++ lib/data/hive_repository/repository/hive_repository.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_app/data/common/repository_error.dart';
 import 'package:note_app/data/hive_repository/dao/hive_note_dao.dart';
+import 'package:note_app/data/mapper/hive_note_dao_mapper.dart';
 import 'package:note_app/domain/model/note_entity.dart';
 import 'package:note_app/domain/model/result.dart';
 import 'package:note_app/domain/repository/note_repository.dart';
@@ -17,19 +18,26 @@ class HiveRepository implements NoteRepository {
 
   @override
   Future<Result<NoteEntity>> create({required NoteEntity entity}) async {
-    final box = await Hive.openBox(_boxName);
-
-    final dao = HiveNoteDAO(
-        id: entity.id,
-        backgroundColorHex: entity.backgroundColorHex,
-        fontColorHex: entity.fontColorHex,
-        title: entity.title,
-        content: entity.content);
-
     try {
+      final box = await Hive.openBox<HiveNoteDAO>(_boxName);
+      final dao = HiveNoteDAO(
+          id: entity.id,
+          backgroundColorHex: entity.backgroundColorHex,
+          fontColorHex: entity.fontColorHex,
+          title: entity.title,
+          content: entity.content);
+
       await box.add(dao);
+
+      final mapper = HiveNoteDAOMapper();
+      final resultList = box.values.map((e) => mapper.mapperTo(e)).toList();
+
+      _streamController.add(Result.success(resultList));
+
       return Result.success(entity);
     } catch (_) {
+      _streamController.add(Result.error(RepositoryError.fetch.description));
+
       return Result.error(RepositoryError.create.description);
     }
   }
@@ -40,7 +48,18 @@ class HiveRepository implements NoteRepository {
   }
 
   @override
-  void fetch() {}
+  void fetch() async {
+    try {
+      final box = await Hive.openBox<HiveNoteDAO>(_boxName);
+
+      final mapper = HiveNoteDAOMapper();
+      final resultList = box.values.map((e) => mapper.mapperTo(e)).toList();
+
+      _streamController.add(Result.success(resultList));
+    } catch (_) {
+      _streamController.add(Result.error(RepositoryError.fetch.description));
+    }
+  }
 
   @override
   Future<Result<NoteEntity>> update(NoteEntity entity) {
